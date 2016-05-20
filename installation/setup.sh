9d3cd6be57fb16259481cc9f49ddc5e0614182ac
Efi Kaltirimidou <kaltirim@lab.grid.auth.gr>
2016-05-20 13:46:00 +0300

    select packages for install

--- installation/setup.sh
+++ installation/setup.sh
@@ -19,6 +19,61 @@ echoinfo() {
   echo -e "[INFO] \e[32m$1 $2 $3 $4\e[0m"
 }
 
+# Function that prints with green colour
+echo_green() {
+  echo -e "\e[32m$1 $2 $3 $4\e[0m"
+}
+
+echo_packages(){
+  hadoop=false
+  elastic=false
+  logstash=false
+  supervisord=false
+
+  echo
+  echoinfo "The following packages are required from the Lambda project to run:"
+  echo_green "1. Hadoop version 2.7 or greater."
+  echo_green "2. Elasticsearch version 2.2 or greater."
+  echo_green "3. Logstash version 2.3 or greater."
+  echo_green "4. Supervisord version 2.2 or greater."
+  echo
+  echoinfo "You can select from 0 (none) to 4 (all) packages to be installed."
+  echoinfo "Type the numbers of the packages you want separated by spaces and press [ENTER]."
+  read raw_packages
+
+  packages=$(echo $raw_packages | tr " " "\n")
+
+  for package in $packages
+    do
+      case $package in
+        [1])
+          hadoop=true
+          ;;
+        [2])
+          elastic=true
+          ;;
+        [3])
+          logstash=true
+          ;;
+        [4])
+          supervisord=true
+          ;;
+    esac
+  done
+
+  if ! $hadoop && ! $elastic && ! $logstash && ! $supervisord;
+    then
+    echoinfo "No package selected for installation, the process with terminate now."
+    exit 0;
+  fi
+
+  if $hadoop;
+    then run_ansible $hadoop
+  fi
+
+  
+}
+
 
 # Function to check if Git is present in the system and install it if not.
 git_check() {
@@ -63,9 +118,9 @@ git_check() {
 }
 
 
-# Function to run the ansible script for cluster installation 
+# Function to run the ansible script for cluster installation
 run_ansible() {
-
+  exit 0;
 }
 
 
@@ -105,3 +160,6 @@ command -v ansible >/dev/null 2>&1 ||
 # Print Ansible installed version
 OUT=$(command ansible --version 2>&1 | (head -n1 && tail -n1) | awk '{split($0, array, " "); print array[2]}')
 echoinfo "Ansible version installed " $OUT
+
+# Print packages for installation
+echo_packages
